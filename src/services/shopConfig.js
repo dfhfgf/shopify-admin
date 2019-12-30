e2a2557f18d3fff0cfe77c9d7066838427ef42d3
dfhfgf <1622214696@qq.com>
2019-12-30 20:21:28 +0800

    测试文档

--- src/services/shopConfig.js
+++ src/services/shopConfig.js
@@ -1,3 +1,3 @@
 export const shopName = 'zhangjiann'; //我的商店名字
-export const apiBase = `https://mirror.viralbox.org/${shopName}/admin/api/2019-10`; //api根
+export const apiBase = `https://mirror.viralbox.org/zhangjiann/admin/api/2019-10`; //api根
 export const access_token='f44adbfc640bffc6eede6cd15d22b6c6'; //认证
